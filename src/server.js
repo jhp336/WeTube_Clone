fdc8774683cf94c4467115c05386367cec3c9ff9
unknown <jhp336@naver.com>
2022-01-13 22:45:56 +0900

    header setting

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ const logger = morgan('dev');
 app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use((req, res, next) => {
+    res.header("Cross-Origin-Resource-Policy", "cross-origin");
     res.header("Cross-Origin-Embedder-Policy", "require-corp");
     res.header("Cross-Origin-Opener-Policy", "same-origin");
-    res.header("Cross-Origin-Resource-Policy", "cross-origin");
     next();
 });
 app.use(logger);
