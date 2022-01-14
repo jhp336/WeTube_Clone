afa9780af1ef1ad79805537f879fed496127e196
unknown <jhp336@naver.com>
2022-01-14 18:42:24 +0900

    video duration fix

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -64,7 +64,7 @@ const formatTime = function(seconds){
 };
 
 const handleLoadedMetaData = function(event){
-    console.log(event);
+    console.log(event, video.readyState);
     if(isFinite(video.duration)){
         totalTime.innerText = formatTime(Math.floor(video.duration));
         timeline.max = Math.floor(video.duration);
